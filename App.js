04305095d5bfa5a552e8511e2077aa6007821840
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2018-12-15 22:56:21 -0300

    improving the code

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
-export default class App extends Component {
-	render() {
-		return <HomeScreen />;
-	}
-}
+
+const AppNavigator = createStackNavigator(
+	{
+		Home: HomeScreen
+	},
+	{ initialRouteName: 'Home' }
+);
+
+export default createAppContainer(AppNavigator);
