17c67c59dc7eebbb14f3023a5a4ab0979cc1d75c
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2018-12-16 15:44:25 -0300

    app.js
    adding new screens const.

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import TripsScreen from './src/screens/TripsScreen';
 import TripDetailScreen from './src/screens/TripDetailScreen';
+import AddTripScreen from './src/screens/AddTripScreen';
+import AddPointScreen from './src/screens/AddPointScreen';
 
 const AppNavigator = createStackNavigator(
 	{
 		Home: HomeScreen,
 		Trips: TripsScreen,
-		TripDetail: TripDetailScreen
+		TripDetail: TripDetailScreen,
+		AddTrip: AddTripScreen,
+		AddPoint: AddPointScreen
 	},
 	{ initialRouteName: 'Home' }
 );
