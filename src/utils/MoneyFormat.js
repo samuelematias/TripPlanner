db4bad7b00ea97da93820e469afd32c222d4c7fe
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2018-12-16 21:49:42 -0300

    create new utils, MoneyFormat.

---
+++ src/utils/MoneyFormat.js
@@ -0,0 +1,18 @@
+import VMasker from 'vanilla-masker';
+
+/**
+ * convert/format the money value to unit R$.
+ * @author samuelmataraso
+ * @method MoneyFormat
+ * @param float number
+ * @returns string
+ */
+export default (MoneyFormat = number => {
+	return VMasker.toMoney(number, {
+		precision: 2,
+		separator: ',',
+		delimiter: '.',
+		unit: 'R$',
+		zeroCents: false
+	});
+});
---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+import isIphoneX from './IsIphoneX';
+import MoneyFormat from './MoneyFormat';
+
+export { isIphoneX, MoneyFormat };
