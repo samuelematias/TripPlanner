a2c0a3f3c1c47372c3c5fdac541f90a21cf06cdc
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2018-12-16 21:51:18 -0300

    fix comment message.

--- src/utils/MoneyFormat.js
+++ src/utils/MoneyFormat.js
@@ -4,7 +4,7 @@ import VMasker from 'vanilla-masker';
  * convert/format the money value to unit R$.
  * @author samuelmataraso
  * @method MoneyFormat
- * @param float number
+ * @param string number
  * @returns string
  */
 export default (MoneyFormat = number => {
